d27f246591440d8046bd6ad5788aa3715b74b7b9
Patrick Pissurno <contato@patrickpissurno.com.br>
2020-10-06 09:33:13 -0300

    Initial commit

---
+++ QuickNet/QuickNet/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace QuickNet
+{
+    public static class Main
+    {
+        [DllExport("Square", CallingConvention = CallingConvention.Cdecl)]
+        public static double Square(double a)
+        {
+            return a * a;
+        }
+    }
+}
